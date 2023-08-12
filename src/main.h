5178399707763bc496c40b61ad1c2d15dc0561be
andrew <sniffy@live.com>
2023-08-13 00:28:09 +1000

    Ready to use

--- src/main.h
+++ src/main.h
@@ -6,8 +6,6 @@
 #define REPORT_WAITS 25 /* LCD and USART waits before refresh. */
 #define DEBUG_OUTPUT 0 /* Output additional data via USART. Disable unless testing. */
 
-void fan_init();
-
 void write_usart(uint8_t *ptr);
 void report_data(void);
 void calibration(uint8_t mode);
